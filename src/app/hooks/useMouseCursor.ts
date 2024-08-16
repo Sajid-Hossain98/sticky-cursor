8cd0e9b6531bdfea2d45a54c10378d1d8f45faa6
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-08-16 22:08:22 +0600

    making the cursor a hook

---
+++ src/app/hooks/useMouseCursor.ts
@@ -0,0 +1,37 @@
+import { useMotionValue, useSpring } from "framer-motion";
+import { useEffect } from "react";
+
+export const useMouseCursor = () => {
+  const cursorSize = 25;
+
+  const mouse = {
+    x: useMotionValue(-25),
+    y: useMotionValue(-25),
+  };
+
+  const smoothMouseOptions = {
+    damping: 20,
+    stiffness: 150,
+    mass: 0.5,
+  };
+
+  const smoothMouse = {
+    x: useSpring(mouse.x, smoothMouseOptions),
+    y: useSpring(mouse.y, smoothMouseOptions),
+  };
+
+  const manageMouseMove = (e: MouseEvent) => {
+    const { clientX, clientY } = e;
+    mouse.x.set(clientX - cursorSize / 2);
+    mouse.y.set(clientY - cursorSize / 2);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", manageMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", manageMouseMove);
+    };
+  }, []);
+
+  return smoothMouse;
+};
